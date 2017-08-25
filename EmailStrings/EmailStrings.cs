a84437f3f863953c279f4a1cbc3f274e2496730b
Firefly4266 <gaines.johnp@gmail.com>
2017-08-25 03:09:31 -0700

    still working on regex solution.

--- EmailStrings/EmailStrings.cs
+++ EmailStrings/EmailStrings.cs
@@ -6,17 +6,75 @@ public class EmailStrings
     {
         InputWrapper iw = new InputWrapper();
         char[] spaces = { '@', '.' };
-        string[] elements;
         Console.WriteLine("Enter string, enter to exit.  ");
         string email = iw.getString("email: ");
         while (!email.Equals(""))
         {
-            elements = email.Split(spaces);
-            foreach (string ele in elements)
+            int index = email.IndexOf('@', 0);
+            if(index != -1)
             {
-                Console.WriteLine(ele);
+                string user = email.Substring(0, index);
+                int start = index + 1;
+                index = email.IndexOf('.', index);
+                if(index != -1)
+                {
+                    string domain = email.Substring(start, index - start);
+                    string suffix = email.Substring(index + 1);
+                    if (suffix == "com" || suffix == "org" || suffix == "edu")
+                    {
+                        if(isAlphaNum(user) && isAlphaNum(domain))
+                        {
+                            Console.WriteLine("user = {0}, \n domain = {1}, \n suffix = {2}.", user, domain, suffix);                      
+                        }
+                        else
+                            Console.WriteLine("user and domaim must be alpha numeric");
+                    }
+                    else
+                        Console.WriteLine("{0}, {1}, and {2} are the only leagal suffixes.", "com", "org", "edu" );
+                }
+                else
+                    Console.WriteLine("Missing '.', address invalid.");
             }
-            email = iw.getString("email: ");
+            else
+                Console.WriteLine("Missing '@', address invalid.");
         }
+        Console.WriteLine("Enter string, enter to exit.  ");
+        email = iw.getString("email: ");
+    }
+   
+
+    private static bool isAlphaNum(String domain)
+    {
+        Regex testPattern = new RegularExpression("^[a-zA-Z0-9]*$");
+        return domain.IsMatch(testPattern);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //elements = email.Split(spaces);
+            //foreach (string ele in elements)
+            //{
+            //    Console.WriteLine(ele);
+            //}
